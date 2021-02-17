862f0d068f7815f150ee8e39456a85e12a296001
FredyRomero <fredysebastianromero@gmail.com>
2021-02-17 08:16:34 +0100

    Commit actualiza nombre alcohol

--- models/liquorstore_alcohol.py
+++ models/liquorstore_alcohol.py
@@ -5,12 +5,12 @@ from odoo.exceptions import Warning
 class Alcohol(models.Model):
     _name = 'liquorstore.alcohol'
     _description = 'Alcohol'
-    name = fields.Char('Name', required=True)
+    name = fields.Char('Nombre', required=True)
     marca = fields.Char('Marca')
     descripcion = fields.Char('Descripcion')
+    image = fields.Binary('Image', help="../static/description/imgliquorstore.png")
     active = fields.Boolean('Active?', default=True)
     # date_published = fields.Date()
-    image = fields.Binary('Image', help="../static/description/imgliquorstore.png")
     # publisher_id = fields.Many2one('res.partner', string='Publisher')
     # author_ids = fields.Many2many('res.partner', string='Authors')
     # name = fields.Char(translate=True, required=True)
