05675e06b8900e360a2c1f8e0544c97aaf023ed4
FredyRomero <fredysebastianromero@gmail.com>
2021-02-17 09:50:57 +0100

    En este commit se agrega la selcción de tipo a alcohol

--- models/liquorstore_alcohol.py
+++ models/liquorstore_alcohol.py
@@ -8,17 +8,11 @@ class Alcohol(models.Model):
     name = fields.Char('Nombre', required=True)
     marca = fields.Char('Marca')
     descripcion = fields.Char('Descripcion')
-    image = fields.Binary('Image', help="../static/description/imgliquorstore.png")
+    image = fields.Binary('Image')
     active = fields.Boolean('Active?', default=True)
-    precio = fields.Integer()
+    precio = fields.Integer('Precio')
     # date_published = fields.Date()
     # publisher_id = fields.Many2one('res.partner', string='Publisher')
     # author_ids = fields.Many2many('res.partner', string='Authors')
     # name = fields.Char(translate=True, required=True)
-
-    # Hierarchy fields
-    parent_id = fields.Many2one(
-        'liquorstore.alcohol.tipo',
-        'Parent Category',
-        ondelete='restrict')
-    parent_path = fields.Char(index=True)
+    tipos_id = fields.Many2one('liquorstore.alcohol.tipo', string='Tipo')
--- models/liquorstore_alcohol_tipo.py
+++ models/liquorstore_alcohol_tipo.py
@@ -4,21 +4,11 @@ class AlcoholTipo(models.Model):
     _name = 'liquorstore.alcohol.tipo'
     _description = 'Alcohol Category'
     _parent_store = True
-    name = fields.Char('Name', required=True)
+    name = fields.Char('Nombre', required=True)
 
     parent_id = fields.Many2one(
-        'liquorstore.alcohol.tipo',
+        'liquorstore.alcohol',
         'Parent Category',
-        ondelete='restrict')
+        ondelete='cascade')
     parent_path = fields.Char(index=True)
 
-    # Optional but good to have:
-    child_ids = fields.One2many(
-        'liquorstore.alcohol.tipo',
-        'parent_id',
-        'Subcategories')
-
-    highlighted_id = fields.Reference(
-        [('liquorstore.alcohol.tipo', 'Alcohol'), ('res.partner', 'Author')],
-        'Category Highlight',
-    )
