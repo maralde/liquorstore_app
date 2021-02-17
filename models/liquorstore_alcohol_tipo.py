af80d31e62511f2afbf9a2c89759c29a18fd2443
miguel-roman <Holandesherrante7>
2021-02-17 10:15:52 +0100

    Cambios visuales agregado(Michu)

--- models/liquorstore_alcohol_tipo.py
+++ models/liquorstore_alcohol_tipo.py
@@ -4,11 +4,21 @@ class AlcoholTipo(models.Model):
     _name = 'liquorstore.alcohol.tipo'
     _description = 'Alcohol Category'
     _parent_store = True
-    name = fields.Char('Nombre', required=True)
+    name = fields.Char('Name', required=True)
 
     parent_id = fields.Many2one(
-        'liquorstore.alcohol',
+        'liquorstore.alcohol.tipo',
         'Parent Category',
-        ondelete='cascade')
+        ondelete='restrict')
     parent_path = fields.Char(index=True)
 
+    # Optional but good to have:
+    child_ids = fields.One2many(
+        'liquorstore.alcohol.tipo',
+        'parent_id',
+        'Subcategories')
+
+    highlighted_id = fields.Reference(
+        [('liquorstore.alcohol.tipo', 'Alcohol'), ('res.partner', 'Author')],
+        'Category Highlight',
+    )
